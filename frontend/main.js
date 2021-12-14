3b32379f424465707a72b8bb7df558546cf601c0
Bidyut Mondal <bidyutfcb@gmail.com>
2021-12-14 22:55:22 +0530

    Final changes to github link in index.html

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', (event)=>{
     getVisitCount();
 })
 
-
+const functionApiUrl = 'https://bidyutgetresumecounter.azurewebsites.net/api/GetResumeCounter?code=oW6LQVpLaGcPnBDfdORXurO98YSj8nNw/5OB/zk0dTaKJHFahcoCZw=='
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API");
