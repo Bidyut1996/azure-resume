6b56b7defd65cb1e49e47d4d51a68ce5738c4082
Bidyut Mondal <bidyutfcb@gmail.com>
2021-12-15 22:18:50 +0530

    Created workflows

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event)=>{
     getVisitCount();
 })
 
-const functionApiUrl = 'https://bidyutgetresumecounter.azurewebsites.net/api/GetResumeCounter?code=oW6LQVpLaGcPnBDfdORXurO98YSj8nNw/5OB/zk0dTaKJHFahcoCZw=='
+const functionApiUrl = 'https://bidyutresumecounter.azurewebsites.net/api/GetResumeCounter?code=Lvw6WxE3caySIOAvB8l6sAr1m0QVTLEU624QfheJvMov1pgfiJ0o4g=='
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
